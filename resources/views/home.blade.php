3843ed71c1583c78c923d828cfc834432a1d07bf
Guillaume Dery <dery.guillaume98@outlook.fr>
2020-02-11 10:37:15 +0100

    Test translate mail + front dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,23 +3,46 @@
 @section('titre', 'Dashboard')
 
 @section('contenu')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<div class="row mt-1">
+    <div class="col-2">
+        <a href="/updateuser"><button type="button" class="btn btn-outline-info waves-effect">Mes infos</button></a>
     </div>
+    <div class="col-2 offset-8">
+        <a href="/createAnnonce"><button type="button" class="btn btn-outline-info waves-effect">Créer
+                annonce</button></a>
+    </div>
+
+</div>
+<div class="row mt-3">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Titre</th>
+                <th scope="col">Code postal</th>
+                <th scope="col">Status</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 @endsection
